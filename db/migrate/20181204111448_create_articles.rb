08b1f825706b0348b2f637269d97d368e076d1d3
Martin <mts13@hotmail.fr>
2018-12-04 12:58:42 +0100

    Added: database (articles, users, favorites, categories)

---
+++ db/migrate/20181204111448_create_articles.rb
@@ -0,0 +1,13 @@
+class CreateArticles < ActiveRecord::Migration[5.2]
+  def change
+    create_table :articles do |t|
+      t.text :description
+      t.integer :size
+      t.boolean :status
+      t.belongs_to :user
+      t.belongs_to :category
+
+      t.timestamps
+    end
+  end
+end
