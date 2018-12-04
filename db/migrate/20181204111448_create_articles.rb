f1cf7f3180fcddedf863efdbba858d10b94bb3b1
mik <autoclik974@gmail.com>
2018-12-04 23:08:53 +0100

    add image to migrate

--- db/migrate/20181204111448_create_articles.rb
+++ db/migrate/20181204111448_create_articles.rb
@@ -2,7 +2,7 @@ class CreateArticles < ActiveRecord::Migration[5.2]
   def change
     create_table :articles do |t|
       t.text :description
-      t.string :image_url
+      t.string :image
       t.integer :size
       t.boolean :status
       t.belongs_to :user
