323639b5222515c68b88d65322c8d34998409e33
mik <autoclik974@gmail.com>
2018-12-04 23:13:02 +0100

    add carrierwv

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,6 @@ class Article < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_one :favorite
+  validates :description, :size, :status, presence: true
+  mount_uploader :image, ImageUploader
 end
