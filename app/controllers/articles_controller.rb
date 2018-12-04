b40c0c322a0acab0fcc63283873927318342fde0
mik <autoclik974@gmail.com>
2018-12-04 17:31:36 +0100

    manip asset

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,13 @@
 class ArticlesController < ApplicationController
+	before_action :authenticate_user!
  def index
+ 	
     @articles = Article.all
     @categories = Category.all
   end
+def create
+	@article = Article.new(articles_params)
+end
 
   def show
     @articles = Article.find(params[:id])
