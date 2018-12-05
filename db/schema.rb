934b963e0f2c97098b7f7a340ac6255ff9774e0a
Martin <mts13@hotmail.fr>
2018-12-05 15:53:22 +0100

    Added: Article list + Redirect to owner on click

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 2018_12_04_120615) do
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
+    t.string "title"
     t.text "description"
     t.integer "size"
     t.boolean "status"
@@ -41,6 +42,7 @@ ActiveRecord::Schema.define(version: 2018_12_04_120615) do
   create_table "messages", force: :cascade do |t|
     t.bigint "user_id"
     t.text "content"
+    t.integer "dest_user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_messages_on_user_id"
@@ -55,6 +57,7 @@ ActiveRecord::Schema.define(version: 2018_12_04_120615) do
     t.boolean "sex"
     t.integer "code_confirm"
     t.string "image"
+    t.boolean "phone_verified"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
