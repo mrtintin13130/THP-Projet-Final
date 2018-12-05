0e3dbba43b4328a1d3a3ae99acf20dff37e1f727
Ahmed <ahmahm@hotmail.co.jp>
2018-12-05 16:02:47 +0100

    merge

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
 	as :user do
 		get 'users/profile', :to => 'devise/registrations#edit', :as => :user_root
 	end
-	>>>>>>> 6769605d02284e2ff4aafc4c0c73b57e9ff2c41d
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
