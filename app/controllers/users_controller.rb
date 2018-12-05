305a6ec6bb0072132d88eb01302faa40caff23cf
nomoon456 <nomoon4567891011@gmail.com>
2018-12-05 19:29:59 +0100

    phone verification

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-	
+before_action :set_user
   def show
     @user = User.find(params[:id])
   end
@@ -8,4 +8,27 @@ class UsersController < ApplicationController
     @user = User.all
     @article = Article.all
   end
+
+  def verification_number
+		User.sms_send(@user, @user.phone)
+	end
+	def new_verification_number
+		if params[:new] != nil
+			@user.phone = params[:new]
+			@user.save
+		elsif params[:text] != nil
+			if params[:text].to_i == @user.code_confirm
+				@user.verified = true
+				@user.save
+			end
+		end
+		respond_to do |format|
+			format.html { redirect_to request.referrer}
+		end
+	end
+	private
+	def set_user
+		puts "bonjour aaaaaaaaaaaa========================"
+		@user = current_user
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,35 +6,29 @@ class User < ApplicationRecord
   :recoverable, :rememberable, :validatable, :confirmable
   has_many :articles
   has_one :favorite
-
-  def numtransform
-    b = User.last
-    if b.phone[0, 2] == "06" || b.phone[0, 2] == "07"
-      b.phone = "33" + b.phone
-      b.phone.slice!(2)
-    end
-    b.save
-    return b
-  end
-  after_create :numtransform
-  def sms_send
-    b = numtransform
-    client = Nexmo::Client.new(
-      api_key: "b5d67031",
-      api_secret: "zGuhQACL1j2OsAO0"
-    )
-    if b.phone[0, 3] == "336" || b.phone[0, 3] == "337"
-      client.sms.send(
-        from: "SWAT",
-        to: b.phone,
-        text: "Bienvenue sur notre super site"
-      )
-    else
-      puts "ntr c pa valid"
-    end
-  end
-
-  after_create :sms_send
   has_many :messages
+  def code_create
+    if user.code_confirm == nil || user.updated_at < DateTime.now - 1.minutes
+      user.code_confirm = rand(1000..9999)
+      user.save
+    end
+    if @text.to_i == user.code_confirm
+      user.phone_verified = true
+      user.save
+    end
+  end
 
+  def self.authenticate(phone)
+    if phone[0, 2] == "06" || phone[0, 2] == "07"
+      phone = "33" + phone
+      phone.slice!(2)
+    end
+  return phone
+  end
+
+  def self.sms_send(user, phone)
+    authenticate(phone)
+    client = Nexmo::Client.new(api_key: "b5d67031", api_secret: "zGuhQACL1j2OsAO0")
+    client.sms.send(from: "SWAT", to: phone, text: "Voici votre code secret #{user.code_confirm}")
+  end
 end
