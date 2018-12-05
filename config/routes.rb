6769605d02284e2ff4aafc4c0c73b57e9ff2c41d
Martin <mts13@hotmail.fr>
2018-12-05 15:09:30 +0100

    Added: Article show

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   resources :messages
   devise_for :users
   resources :users
-  
+  resources :articles
+
   as :user do
     get 'users/profile', :to => 'devise/registrations#edit', :as => :user_root
   end
