cb632529dde3c3e7dfbc5fbb092065e3b6f9e70e
Martin <mts13@hotmail.fr>
2018-12-07 10:48:08 +0100

    Removed: extra .capitalize

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 	def index
 		@user = current_user
 		if @user != nil
-			@user_names = @user.last_name.capitalize + " " + @user.first_name.capitalize
+			@user_names = @user.last_name + " " + @user.first_name
 		end
 	end
 end
