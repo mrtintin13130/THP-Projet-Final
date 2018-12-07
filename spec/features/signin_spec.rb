cfaceb8b00203403ac4eb19f8b6844de9ab0aa2e
Timmo404 <cala.stephane@gmail.com>
2018-12-07 13:10:55 +0100

    search for button

--- spec/features/signin_spec.rb
+++ spec/features/signin_spec.rb
@@ -3,17 +3,23 @@ require 'rails_helper'
 feature "signing in" do
   let(:user) {FactoryBot.build(:user)}
 
-  def fill_in_signin_field
-    fill_in "user[email]", with: user.email
-    fill_in "user[password]", with: user.password
-    fill_in "user[password_confirmation]", with: user.password
-    click_button("Sign up")
+  def fill_in_fields
+    stripe_card_number = Stripe.setPublishableKey()
+
+    fill_in "email", with: user.email
+    find_field('Card number').send_keys(stripe_card_number)
+    find_field('MM / YY').send_keys "01#{DateTime.now.year + 1}"
+    find_field('CVC').send_keys '123'
+    #find(:css, ".Checkb").set(true)
+    #find_field('').send_keys ''
+
+    find('button[type="submit"]').click
   end
 
   scenario "visiting the site to sign in" do
-    visit root_path
-    click_link "S'enregistrer"
-    fill_in_signin_field
-    expect(page).to have_content("Signed in successfuly")
+    visit new_charge_path
+    click_button "stripe-button"
+    fill_in_fields
+    expect(charges_path).to have_content("Thanks, you paid")
   end
 end
