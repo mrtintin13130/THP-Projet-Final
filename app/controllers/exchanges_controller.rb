81a1f6b99ac9b798818220f412ceefc2df8945e8
Martin <mts13@hotmail.fr>
2018-12-07 13:46:56 +0100

    Improved: Trading system

--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -3,6 +3,7 @@ class ExchangesController < ApplicationController
   def index
     @asked_exchange = Exchange.where(applicant_user_id: current_user.id)
     @my_exchange = Exchange.where(owner_user_id: current_user.id)
+    @exchange = Exchange.all
     @article = Article.all
     @user = User.all
   end
@@ -11,14 +12,20 @@ class ExchangesController < ApplicationController
     exchanges = Exchange.all
     article_id = params[:article_id]
     owner_id = params[:owner_id]
-    trade_exist = exchanges.find_by(owner_user_id: owner_id, applicant_user_id: Article.find(article_id).user_id)
+    trade_exist = exchanges.find_by(owner_user_id: current_user.id, applicant_user_id: owner_id)
+    puts "AAAAAAAAAAAAAAAAAAAAAAAAAA"
+    puts trade_exist.blank?
     if trade_exist.blank?
-      puts "AAAAAAAAAAA"
       new = Exchange.new(applicant_user_id: current_user.id, owner_user_id: owner_id, owner_article_id: article_id)
       new.save
     else
+      puts "CCCCCCCCCCCCCCCCCCC"
       trade_exist.update(applicant_article_id: article_id)
     end
     redirect_to request.referer
+  end
+
+  def show
+
   end
 end
