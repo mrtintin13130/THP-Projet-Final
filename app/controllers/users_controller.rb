98c2087d93b937c0560a5ed6923865b85bce3dcf
Martin <mts13@hotmail.fr>
2018-12-07 10:52:12 +0100

    Trying on heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,9 @@
 		else
 			@user = User.find(params[:id])
 		end
-		if @user != nil
-			@user_names = @user.last_name + " " + @user.first_name
-		end
+		# if @user != nil
+		# 	@user_names = @user.last_name + " " + @user.first_name
+		# end
 	end
 
 	def index
