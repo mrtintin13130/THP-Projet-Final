bb23fd10a5d353724408387ab5722da7d4caca35
Timmo404 <cala.stephane@gmail.com>
2018-12-11 14:08:40 +0100

    00

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# 5.times do
-# 	Category.create(name: Faker::Football.competition)
-# end
+5.times do
+	Category.create(name: Faker::Football.competition)
+end
 
-# 10.times do
-# 	user = User.new(email: Faker::Internet.email, password: "testtest", password_confirmation: "testtest", first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
-# 	user.save!
-# end
+10.times do
+	user = User.new(email: Faker::Internet.email, password: "testtest", password_confirmation: "testtest", first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+	user.save!
+end
 
-# 15.times do
-# 	Article.create(title: Faker::Esport.game, description: Faker::LeagueOfLegends.quote, size: Faker::Number.between(30, 45), status: true, user_id: Faker::Number.between(1, 10), category_id: Faker::Number.between(1, 5))
-# end
+15.times do
+	Article.create(title: Faker::Esport.game, description: Faker::LeagueOfLegends.quote, size: Faker::Number.between(30, 45), status: true, user_id: Faker::Number.between(1, 10), category_id: Faker::Number.between(1, 5))
+end
 
 
 	#Category.create(name: "Tshirt")
