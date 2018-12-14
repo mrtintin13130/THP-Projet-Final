6981d376920093309ab95f904c407d636c76d1b2
Martin <mts13@hotmail.fr>
2018-12-14 13:24:58 +0100

    Added: Unread messages count

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,20 +6,7 @@ class MessagesController < ApplicationController
   def index
     @user = current_user
     @messages = @conversation.messages
-    if @messages.length > 10
-      @over_ten = true
-      @messages = @messages[-10..-1]
-    end
-    if params[:m]
-      @over_ten = false
-      @messages = @conversation.messages
-    end
-    if @messages.last
-      if @messages.last.user_id != current_user.id
-        @messages.last.read = true;
-      end
-    end
-
+    @conversation.messages.where("user_id != ? AND read = ?", current_user.id, false).update_all(read: true)
     @message = @conversation.messages.new
   end
 
