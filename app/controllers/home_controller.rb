0c88fce64455b3cc2c7025e27478dd0c83fd5f9a
Ahmed <ahmahm@hotmail.co.jp>
2018-12-15 20:36:12 +0100

    coeur image

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
 	def index
+		@exchanges_sum = Exchange.all.count
 		@messages_sum = Message.all.count
 		@users_sum = User.all.count
 		@articles_sum = Article.all.count
