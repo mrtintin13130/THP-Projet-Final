783209afdd5a738c82b3a0a91a7264bc65993935
Ahmed <ahmahm@hotmail.co.jp>
2018-12-16 18:03:59 +0100

    fix fix fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,14 @@
 
 puts "AJOUT CATEGORY"
 
-# Category.create(name: "pantalon")
-# Category.create(name: "sweat")
-# Category.create(name: "chaussure")
-# Category.create(name: "lunette")
-# Category.create(name: "casquette")
-# Category.create(name: "chaussette")
-# Category.create(name: "veste")
-# Category.create(name: "echarpe")
+Category.create(name: "pantalon")
+Category.create(name: "sweat")
+Category.create(name: "chaussure")
+Category.create(name: "lunette")
+Category.create(name: "casquette")
+Category.create(name: "chaussette")
+Category.create(name: "veste")
+Category.create(name: "echarpe")
 Category.create(name: "tshirt")
 Category.create(name: "bracelet")
 Category.create(name: "montre")
@@ -30,30 +30,30 @@ puts "Done !"
 
 
 
-# puts "AJOUT USER"
-# 10.times do
-# 	user = User.new(email: Faker::Internet.email, password: "testtest", password_confirmation: "testtest", first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, sex: Faker::Gender.binary_type, username: Faker::Name.middle_name, city: Faker::Address.city, username: Faker::FunnyName.name)
-# 	user.save!
-# end
-# puts "Done !"
+puts "AJOUT USER"
+20.times do
+	user = User.new(email: Faker::Internet.email, password: "testtest", password_confirmation: "testtest", first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, sex: Faker::Gender.binary_type, username: Faker::Name.middle_name, city: Faker::Address.city, username: Faker::FunnyName.name)
+	user.save!
+end
+puts "Done !"
 
 
 
 puts "AJOUT ARTICLES"
 
-15.times do
-	Article.create(title: Faker::App.name, description: Faker::Lorem.paragraph, size: Faker::Number.between(30, 45), status: true, user_id: Faker::Number.between(1, 10), category_id: Faker::Number.between(1, 16) )
+50.times do
+	Article.create(title: Faker::App.name, description: Faker::Lorem.paragraph, size: Faker::Number.between(30, 45), status: true, user_id: Faker::Number.between(1, 20), category_id: Faker::Number.between(1, 16) )
 end
 
 
 puts "Done!"
 
-puts "AJOUT ECHANGES"
+# puts "AJOUT ECHANGES"
 
-15.times do
-	Exchange.create(applicant_user_id: Faker::Number.between(1, 10), owner_user_id: Faker::Number.between(1, 10), applicant_article_id: Faker::Number.between(1, 15), owner_article_id: Faker::Number.between(1, 15), status: true )
-end
-puts "Done!"
+# 15.times do
+# 	Exchange.create(applicant_user_id: Faker::Number.between(1, 10), owner_user_id: Faker::Number.between(1, 10), applicant_article_id: Faker::Number.between(1, 15), owner_article_id: Faker::Number.between(1, 15), status: true )
+# end
+# puts "Done!"
 
 
 puts "AJOUT MESSAGES"
