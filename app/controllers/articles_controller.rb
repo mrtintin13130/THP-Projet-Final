494a1f99da04d4edd3ccfbf6c09c50b17d370256
Ahmed <ahmahm@hotmail.co.jp>
2018-12-16 18:12:29 +0100

    delete art user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -59,7 +59,7 @@ class ArticlesController < ApplicationController
   @article.likes.delete_all
   @article.destroy
   respond_to do |format|
-    format.html { redirect_to request.referrer, notice: "Cet article, ainsi que ses likes, ont été supprimés" }
+    format.html { redirect_to root_path, notice: "Cet article, ainsi que ses likes, ont été supprimés" }
   end
 end
 
