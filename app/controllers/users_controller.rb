57cb04e9f7a8211c400ef264ad7d711f6907c5eb
Ahmed <ahmahm@hotmail.co.jp>
2018-12-16 17:05:30 +0100

    fix profil

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController
-
+  before_action :set_user
   before_action :set_username
 
   def show
     @user = User.find(params[:id])
+    if user_signed_in?
+      if @user.id == current_user.id
+        @user = current_user
+      else
+        @user = User.find(params[:id])
+      end
+    end
     @mes_likes = Like.where(user_id: current_user.id)
     @exchanges = Exchange.all
     @user_exchanges = @exchanges.where(applicant_user_id: @user.id).where(status: true)
@@ -17,53 +24,44 @@ class UsersController < ApplicationController
     @followers = Favorite.where(favorite_user_id: @user.id).count
   end
 
-  def profil
 
-    if user_signed_in? 
-      @user = current_user
-    else
-     respond_to do |format|
-      format.html { redirect_to new_user_session}
+  def index
+    if user_signed_in?
+      if @user.id == current_user.id
+        @user = current_user
+      else
+        @user = User.find(params[:id])
+      end
+    end
+    @users = User.all
+    @article = Article.all
+    @favorite = Favorite.new
+  end
+
+  def verification_number
+    @phoneuser = User.find(current_user.id).phone
+    if @user.phone_verified == false
+      User.sms_send(@user, @user.phone)
     end
   end
-  @mes_likes = Like.where(user_id: @user.id)
-  @followers = Favorite.where(favorite_user_id: @user.id).count
 
-end
-
-
-
-def index
-  @user = current_user
-  @users = User.all
-  @article = Article.all
-  @favorite = Favorite.new
-end
-
-def verification_number
-  @phoneuser = User.find(current_user.id).phone
-  if @user.phone_verified == false
-    User.sms_send(@user, @user.phone)
-  end
-end
-
-def new_verification_number
-  if params[:new] != nil
-    @user.phone = params[:new]
-    @user.save
-  elsif params[:text] != nil
-    if params[:text].to_i == @user.code_confirm
-      @user.phone_verified = true
+  def new_verification_number
+    if params[:new] != nil
+      @user.phone = params[:new]
       @user.save
+    elsif params[:text] != nil
+      if params[:text].to_i == @user.code_confirm
+        @user.phone_verified = true
+        @user.save
+      end
+    end
+    respond_to do |format|
+      format.html { redirect_to request.referrer}
     end
   end
-  respond_to do |format|
-    format.html { redirect_to request.referrer}
-  end
-end
 
 
-def destroy
+  def destroy
   @user = User.find(params[:id])
   @user.likes.delete_all
   @user.articles.delete_all
@@ -75,11 +73,12 @@ def destroy
   end
 end
 
-private
+
+    private
 
 
-def set_user
-  @user = current_user
-end
+    def set_user
+      @user = current_user
+    end
 
-end
+  end
